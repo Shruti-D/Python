3c10ffb86d20f9bab5c024d12028056e6ceff417
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2021-09-28 09:14:25 +0530

    Update Ex2.py

--- Basics/Ex2.py
+++ Basics/Ex2.py
@@ -1,6 +1,6 @@
 #Python Program to check if a Number Is Positive Or Negative.
 num = int(input("Enter a Number:"))
-if(num%2==0):
+if num%2==0:
     print("Number is Even.")
 else:
     print("Number is Odd")
