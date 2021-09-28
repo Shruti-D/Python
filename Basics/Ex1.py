3e816d23dd4436b2161a3c968a2ef275dc44967b
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2021-09-28 09:02:18 +0530

    Create Ex1.py

---
+++ Basics/Ex1.py
@@ -0,0 +1,8 @@
+'''Python Program to check if a Number Is Positive Or Negative.'''
+num = int(input("Enter a Number:"))
+if(num>0):
+    print("Number is Positive.")
+elif(num==0):
+    print("Number is neither Negative nor Positive.")
+else:
+    print("Number is Negative.")
