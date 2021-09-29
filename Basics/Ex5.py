ee7d8084958cd7bde8a726a454564dacf704c59a
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2021-09-29 06:56:18 +0530

    Create Ex5.py

---
+++ Basics/Ex5.py
@@ -0,0 +1,9 @@
+#Python Program to Find Greatest of Two Numbers.
+n1 = int(input("Enter First Number:"))
+n2 = int(input("Enter Second Number:"))
+if n1>n2:
+    print("The Greatest Number is",n1)
+elif n2>n1:
+    print("The Greatest Number is",n2)
+else:
+    print("Both are Equal.")
