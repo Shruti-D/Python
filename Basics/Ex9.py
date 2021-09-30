53fc0913057e9105f7c1501f0832f4e864a977e9
Shruti-D <65996050+Shruti-D@users.noreply.github.com>
2021-09-30 07:57:07 +0530

    Update Ex9.py

--- Basics/Ex9.py
+++ Basics/Ex9.py
@@ -6,5 +6,11 @@ def isPrime(n):
         print(n)
 start = int(input("Enter Start of the Range:"))
 end = int(input("Enter End of the Range:"))
-for i in range(start,end+1):
-    isPrime(i)
+if (start<1 or end<=1):
+    print("No Prime Numbers.")
+else:
+    print("Prime Number from",start,"to",end,":")
+    for i in range(start,end+1):
+        if(i>1):
+            isPrime(i)
+    
